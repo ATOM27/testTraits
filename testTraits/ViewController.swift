27066f550cad946b1cc0cc1a3c184e3c0ede2207
ATOM27 <eugene.mekhedov@gmail.com>
2019-09-23 09:42:30 +0300

    no message

---
+++ testTraits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testTraits
+//
+//  Created by Eugene  Mekhedov on 15.06.2018.
+//  Copyright © 2018 Eugene  Mekhedov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
